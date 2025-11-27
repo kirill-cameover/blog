572bab556e4684c8f47943b801944b4a09f7650a
Lirikciolac <kiril.ciolac@gmail.com>
2025-11-27 12:15:20 +0300

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,6 +45,21 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     // Component.Backlinks(),
   ],
+  header: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
+    Component.Explorer(),
+  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
