91a21f4d02121ad4b3ba11b55583ba8d060d34b6
Lirikciolac <kiril.ciolac@gmail.com>
2025-11-28 16:42:11 +0300

    layout

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -241,14 +241,13 @@ export function renderPage(
   const Header = HeaderConstructor()
   const Body = BodyConstructor()
 
-  const LeftComponent =
-    left.length > 0 ? (
-      <div class="left sidebar">
-        {left.map((BodyComponent) => (
-          <BodyComponent {...componentData} />
-        ))}
-      </div>
-    ) : null
+  const LeftComponent = (
+    <div class="left sidebar">
+      {left.map((BodyComponent) => (
+        <BodyComponent {...componentData} />
+      ))}
+    </div>
+  )
 
   const RightComponent = (
     <div class="right sidebar">
